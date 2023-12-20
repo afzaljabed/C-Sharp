e0c60537690c2ab38cee63053862cd6551ec7ec1
afzaljabed <afazal.jabed.24@gmail.com>
2023-12-21 00:58:12 +0600

    class 1 basic syntax

---
+++ DevSkill/Batch-08/Examples/01_BasicSyntax/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_BasicSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x = 5;
+            int y = 10;
+            // variable can have alphabet, digit or underscore and it must start with either alphabet or underscore
+            int studentAge = 23;
+            string studentName = "Jalaluddin";
+            bool isAdmitted = true;
+            double weight = 83.5;
+            char initial = 'A';
+
+            string line = Console.ReadLine();
+
+            string[] lineParts = line.Split(' ');
+
+            for(int i = 0; i < lineParts.Length; i++)
+            {
+                Console.WriteLine(lineParts[i]);
+            }
+        }
+    }
+}
